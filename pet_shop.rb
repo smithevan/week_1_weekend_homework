bdc15f0158d31fc07f4e109c47bc32ba267394ec
Evan Smith <smith.m.evan@gmail.com>
2019-11-22 13:39:05 +0000

    pass test 3

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,7 @@ end
 def total_cash(name)
   return name[:admin][:total_cash]
 end
+
+def add_or_remove_cash(name, number)
+  return name[:admin][:total_cash] = name[:admin][:total_cash] + number
+end
